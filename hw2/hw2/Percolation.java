b4e547d27129c41eedef63d0c84d611903863e2f
ycjiang9611@gmail.com <ycjiang9611@gmail.com>
2021-12-29 15:58:57 -0800

    hw2 version1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,103 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+/** UF(int N) initialize union-find data structure with N objects (0-N-1)
+ * void union(int p, int q) add connection between p and q
+ * boolean connected(int p, int q) are p and q in the same component?
+ * int find(int p) component identifier for p
+ * int count() number of components
+*/
 
 public class Percolation {
+    private int size;
+    private boolean[][] arr;
+    private int cnt;
+    private WeightedQuickUnionUF uf; // create elements 0~N^2-1
+    private int top;
+    private int bottom;
+
+    private int twoDToOneD(int r, int c) {
+        return r*size + c;
+    }
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        size = N;
+        arr = new boolean[size][size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                arr[i][j] = false;
+            }
+        }
+        cnt = 0;
+        top = size*size;
+        bottom = size*size + 1;
+        uf = new WeightedQuickUnionUF(size*size + 2);
+        // virtual top site connected to the top row
+        for (int k = 0; k < size; k += 1) {
+            uf.union(top, k);
+        }
+        // virtual bottom site connected to the bottom row
+        for (int k = twoDToOneD(size-1, 0); k < size*size; k += 1) {
+            uf.union(bottom, k);
+        }
+    }
+
+    public void open(int row, int column) {
+        if (row < 0 || row > size-1 || column < 0 || column > size-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (!isOpen(row, column)) {
+            arr[row][column] = true;
+            cnt += 1;
+            // check upper
+            if (row - 1 >= 0 && isOpen(row - 1, column)) {
+                uf.union(twoDToOneD(row, column), twoDToOneD(row - 1, column));
+            }
+            // check lower
+            if (row + 1 < size && isOpen(row + 1, column)) {
+                uf.union(twoDToOneD(row, column), twoDToOneD(row + 1, column));
+            }
+            // check left
+            if (column - 1 >= 0 && isOpen(row, column - 1)) {
+                uf.union(twoDToOneD(row, column), twoDToOneD(row, column - 1));
+            }
+            // check right
+            if (column + 1 < size && isOpen(row, column + 1)) {
+                uf.union(twoDToOneD(row, column), twoDToOneD(row, column + 1));
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int column) {
+        if (row < 0 || row > size-1 || column < 0 || column > size-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return arr[row][column];
+    }
+
+    public boolean isFull(int row, int column) {
+        if (row < 0 || row > size-1 || column < 0 || column > size-1) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (isOpen(row, column)) {
+            return uf.connected(twoDToOneD(row, column), top);
+        }
+        return false;
+    }
+
+    public int numberOfOpenSites() {
+        return cnt;
+    }
+
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,45 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private double[] sample;
+    private int size;
+    private int times;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        size = N;
+        times = T;
+        sample = new double[T];
+        int i = 0;
+        while (i < T) {
+            Percolation pc = pf.make(size);
+            while (!pc.percolates()) {
+                pc.open(StdRandom.uniform(size), StdRandom.uniform(size));
+            }
+            sample[i] = pc.numberOfOpenSites()*1.0/(size*size);
+            i += 1;
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(sample);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(sample);
+    }
+
+    public double confidenceLow() {
+        return mean()-1.96*stddev()/Math.sqrt(times);
+    }
+
+    public double confidenceHigh() {
+        return mean()+1.96*stddev()/Math.sqrt(times);
+    }
 
 }
